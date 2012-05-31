6d58997dfab79e35b1ab490d3e245d6c55e68fb0
George Malamidis <george@nutrun.com>
2012-05-31 07:52:11 -0500

    docs

--- main.go
+++ main.go
@@ -10,18 +10,19 @@ import (
 
 var listener *bool = flag.Bool("listen", false, "Start listener")
 var help *bool = flag.Bool("help", false, "Show help")
-var mailto *string = flag.String("mailto", "", "Who to email on failure (comma separated)")
-var workdir *string = flag.String("workdir", "/tmp", "Directory to run job from")
-var out *string = flag.String("out", "/dev/null", "File to send job's stdout and stderr")
-var tube *string = flag.String("tube", "", "Beanstalkd tube to send the job to")
+var mailto *string = flag.String("mailto", "", "Who to email on failure (comma separated) [sumbit]")
+var workdir *string = flag.String("workdir", "/tmp", "Directory to run job from [submit]")
+var out *string = flag.String("out", "/dev/null", "File to send job's stdout and stderr [submit]")
+var tube *string = flag.String("tube", "", "Beanstalkd tube to send the job to [submit]")
 var stats *bool = flag.Bool("stats", false, "Show queue stats")
 var drain *string = flag.String("drain", "", "Empty tubes (comma separated)")
 var verbose *bool = flag.Bool("v", false, "Increase verbosity")
-var exclude *string = flag.String("exclude", "", "Tubes to ignore (comma separated)")
-var priority *int = flag.Int("pri", 0, "Job priority (smaller runs first)")
-var delay *int = flag.Int("delay", 0, "Job delay in seconds")
+var exclude *string = flag.String("exclude", "", "Tubes to ignore (comma separated) [listen]")
+var priority *int = flag.Int("pri", 0, "Job priority (smaller runs first) [submit]")
+var delay *int = flag.Int("delay", 0, "Job delay in seconds [submit]")
 
 func main() {
+	log.SetFlags(0)
 	flag.Parse()
 	if *listener {
 		include := false
