436905c1e09ff00e513c2377558b3e25d2ace554
Ajit <ageorge@drw.com>
2012-06-01 17:40:24 -0500

    Test kill and terminate

--- integration_test.py
+++ integration_test.py
@@ -35,13 +35,25 @@ class TestGlowIntegration(unittest.TestCase):
         listener.send_signal(SIGINT)
 
     def test_listener_finishes_job_on_interrupt(self):
-        pass
+        tmpfilename = temporary_file_name()
+        listener = start_listener()
+        subprocess.check_call([glow_executable(), '-tube', 'listener_finishes_job_on_interrupt', '-out', tmpfilename, sibling_path('sleepthenecho'), '5',  'listener_finishes_job_on_interrupt'])
+        listener.send_signal(SIGINT)
+        self.wait_for_condition(lambda: os.path.exists(tmpfilename) and os.stat(tmpfilename).st_size > 0, seconds=10)
+        with open(tmpfilename, 'r') as outfile:
+            self.assertEqual('listener_finishes_job_on_interrupt\n', outfile.read())
 
     def test_listener_kills_job_on_kill(self):
-        pass
+        tmpfilename = temporary_file_name()
+        listener = start_listener()
+        subprocess.check_call([glow_executable(), '-tube', 'listener_kills_job_on_kill', '-out', tmpfilename, sibling_path('sleepthenecho'), '5',  'listener_kills_job_on_kill'])
+        listener.terminate()
+        listener.wait()
+        with open(tmpfilename, 'r') as outfile:
+            self.assertNotEqual('listener_kills_job_on_kill\n', outfile.read())
     
-    def wait_for_condition(self, cond_f):
-        end_time = time.time() + 3 # seconds
+    def wait_for_condition(self, cond_f, seconds=3):
+        end_time = time.time() + seconds
         while time.time() < end_time:
             if cond_f():
                 return
@@ -49,7 +61,7 @@ class TestGlowIntegration(unittest.TestCase):
         self.fail('timed out')
 
 
-debug = False
+debug = True
 
 def start_listener():
     return subprocess.Popen([glow_executable(), '-listen'], stderr=None if debug else open('/dev/null', 'r'))
@@ -61,6 +73,10 @@ def temporary_file_name():
         return tmpfilename
     else:
         return tempfile.NamedTemporaryFile().name
+
+def sibling_path(filename):
+    return os.path.join(os.path.dirname(__file__), filename)
+    
 
 def glow_executable():
     return os.path.join(os.path.dirname(__file__), 'glow')
