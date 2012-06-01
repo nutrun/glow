f9b6e08530301750dd90a8836e9bfe75d87aab1f
Ajit <ageorge@drw.com>
2012-06-01 16:50:35 -0500

    don't assume current directory for integration tests (fix build?)

--- integration_test.py
+++ integration_test.py
@@ -12,7 +12,7 @@ class TestGlowIntegration(unittest.TestCase):
     def test_listener_runs_job(self):
         tmpfilename = temporary_file_name()
         listener = start_listener()
-        subprocess.check_call(['./glow', '-tube', 'listener_runs_job', '-out', tmpfilename, '/bin/echo', 'listener_runs_job'])
+        subprocess.check_call([glow_executable(), '-tube', 'listener_runs_job', '-out', tmpfilename, '/bin/echo', 'listener_runs_job'])
         self.wait_for_condition(lambda: os.path.exists(tmpfilename) and os.stat(tmpfilename).st_size > 0)
         with open(tmpfilename, 'r') as outfile:
             self.assertEqual('listener_runs_job\n', outfile.read())
@@ -22,7 +22,7 @@ class TestGlowIntegration(unittest.TestCase):
         tmpfilename1 = temporary_file_name()
         tmpfilename2 = temporary_file_name()
         listener = start_listener()
-        glow = subprocess.Popen(['./glow'], stdin=subprocess.PIPE)
+        glow = subprocess.Popen([glow_executable()], stdin=subprocess.PIPE)
         print >>glow.stdin, cjson.encode([{'cmd': 'echo submit_many_jobs', 'tube': 'submit_many_jobs', 'out': tmpfilename1 },
                                           {'cmd': 'echo submit_many_jobs', 'tube': 'submit_many_jobs', 'out': tmpfilename2 }])
         glow.stdin.close()
@@ -52,7 +52,7 @@ class TestGlowIntegration(unittest.TestCase):
 debug = False
 
 def start_listener():
-    return subprocess.Popen(['./glow', '-listen'], stderr=None if debug else open('/dev/null', 'r'))
+    return subprocess.Popen([glow_executable(), '-listen'], stderr=None if debug else open('/dev/null', 'r'))
 
 def temporary_file_name():
     if debug:
@@ -61,3 +61,6 @@ def temporary_file_name():
         return tmpfilename
     else:
         return tempfile.NamedTemporaryFile().name
+
+def glow_executable():
+    return os.path.join(os.path.dirname(__file__), 'glow')
